23bab3cd551861cca9d4ddfe57b1a22e1174a0e8
mdquayle <misternopants@gmail.com>
2021-05-18 18:58:52 -0400

    Added all the basis of the HTML

---
+++ script.js
@@ -0,0 +1,17 @@
+function rotatePigOne(){
+    let img = document.getElementById('pigOne');
+    img.style.transform = 'rotate(180deg)';
+}
+function rotatePigTwo(){
+    let img = document.getElementById('pigTwo');
+    img.style.transform = 'rotate(180deg)';
+}
+function rotatePigThree(){
+    let img = document.getElementById('pigThree');
+    img.style.transform = 'rotate(180deg)';
+}
+
+function boldText(){
+    let text= document.getElementById('text');
+    text.style.textDecoration = 'underline'
+}
